7ea8f28a0cbd51a90097c6f1e63facbdfe374e03
ChopinF3 <migueljr.alejandro190@gmail.com>
2021-07-01 15:24:18 +0800

    work section created

--- src/components/works.js
+++ src/components/works.js
@@ -2,64 +2,72 @@ function Works() {
   return (
     <section className='container'>
       <h1>
-        <span className='color__orange'>.about</span>
+        <span className='color__orange'>.works</span>
         <span className='color__yellow'> &#123;</span>
       </h1>
-      <div className='row'>
-        <div className='col-md-5 offset-md-1'>
-          <br></br>
-          <p>
-            I’m a Front-End Developer located in Taguig, Philippines. I have a
-            serious passion on frontend staff like UI, animations, and dynamic
-            user experiences.
-          </p>
-          <br></br>
-          <p>
-            Creative visionary, problem solver, committed to self-development.
-            Fan of anime, volleyball, TV series, Music and Movies.
-          </p>
-          <br></br>
-          <p>
-            Interested in the entire frontend spectrum and working on ambitious
-            projects with positive people.
-          </p>
-          <br></br>
+      <br />
+      <div className='works-container'>
+        <div className='row'>
+          <div className='col order-left'>
+            <div className='col-md-0 offset-md-2'>
+              <h1>
+                <span>klase: </span>
+              </h1>
+            </div>
+            <div className='col-md-0 offset-md-2'>
+              <br></br>
+              <p>
+                <span className='color__grey'>
+                  /*e-learning web application*/{' '}
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className='project-container col-md-7 offset-md-0'>
+            <div className='klase-tumbnail tumbnail-size'>&nbsp;</div>
+          </div>
         </div>
-        <div className='col-md-5 offset-md-1'>
-          <br></br>
-          <h1>
-            <span className='color__yellow'>JS</span> vanilla
-          </h1>
-          <h1>
-            <span className='color__yellow'>JS</span> react
-          </h1>
-          <h1>
-            <span className='color__orange'>&#60;&#62;</span> html
-          </h1>
-          <h1>
-            <span className='color__blue'>&nbsp;&#35;</span> css
-          </h1>
-          <h1>
-            <span className='color__yellow'>JS</span> gsap
-          </h1>
-          <h1>
-            <span className='color__yellow'>&#123;&#125;</span> json
-          </h1>
-          <h1>
-            <span className='color__yellow'>JS</span> es6
-          </h1>
-          <h1>
-            <span className='color__pink'>&nbsp;S</span> sass
-          </h1>
+        <div className='row'>
+          <div className='project-container col-md-7 offset-md-0'>
+            <div className='encryptee-tumbnail tumbnail-size'>&nbsp;</div>
+          </div>
+          <div className='col'>
+            <div className='col-md-0 offset-md-2'>
+              <br></br>
+              <h1>
+                <span>encryptee: </span>
+              </h1>
+            </div>
+            <div className='col-md-0 offset-md-2'>
+              <p>
+                <span className='color__grey'>
+                  /*e-commerse web application*/{' '}
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col order-left'>
+            <div className='col-md-0 offset-md-2'>
+              <h1>
+                <span>anydo: </span>
+              </h1>
+            </div>
+            <div className='col-md-0 offset-md-4'>
+              <br></br>
+              <p>
+                <span className='color__grey'>/*simple task management*/ </span>
+              </p>
+            </div>
+          </div>
+          <div className='project-container col-md-7 offset-md-0'>
+            <div className='anydo-tumbnail tumbnail-size'>&nbsp;</div>
+          </div>
         </div>
       </div>
-      <div className='row'>
-        <h1 className='color__yellow col-md-1 offset-md-0'> &#125;</h1>
-        <div className='col col-md-11 offset-md-0'>
-          <p className='color__grey'>&#47;&#47; do what you love</p>
-          <p className='color__grey'>&#47;&#47; love what you do</p>
-        </div>
-      </div>
+      <br />
+      <h1 className='color__yellow col-md-1 offset-md-0'> &#125;</h1>
     </section>
   )
 }
