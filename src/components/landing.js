066ed5a9969d74dd8c45b394d7ebfc6c5df4d9f1
ChopinF3 <migueljr.alejandro190@gmail.com>
2021-07-01 22:41:34 +0800

    skew (media quiery bug)

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,17 +1,68 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import windowSize from '../hooks/useWindowSize'
 import Loading from './loading'
 import Home from './home'
 
 function Landing() {
   const [loading, setLoading] = useState(false)
+  const size = windowSize()
+
+  const app = useRef()
+  const scrollContainer = useRef()
+
+  const data = {
+    ease: 0.1,
+    current: 0,
+    previous: 0,
+    rounded: 0,
+  }
+
+  const setBodyHeight = () => {
+    document.body.style.height = `${
+      scrollContainer.current.getBoundingClientRect().height
+    }px`
+  }
+
+  const skewScrolling = () => {
+    //Set Current to the scroll position amount
+    data.current = window.scrollY
+    // Set Previous to the scroll previous position
+    data.previous += (data.current - data.previous) * data.ease
+    // Set rounded to
+    data.rounded = Math.round(data.previous * 100) / 100
+
+    // Difference between
+    const difference = data.current - data.rounded
+    const acceleration = difference / size.width
+    const velocity = +acceleration
+    const skew = velocity * 7.5
+
+    //Assign skew and smooth scrolling to the scroll container
+    scrollContainer.current.style.transform = `translate3d(0, -${data.rounded}px, 0) skewY(${skew}deg)`
+
+    //loop vai raf
+    requestAnimationFrame(() => skewScrolling())
+  }
 
   useEffect(() => {
     setLoading(true)
     setTimeout(() => {
       setLoading(false)
+
+      requestAnimationFrame(() => skewScrolling())
+      setBodyHeight()
     }, 8000)
   }, [])
 
-  return <>{loading ? <Loading></Loading> : <Home></Home>}</>
+  if (loading) {
+    return <Loading></Loading>
+  }
+  return (
+    <div ref={app} className='app'>
+      <div ref={scrollContainer} className='scroll'>
+        <Home></Home>
+      </div>
+    </div>
+  )
 }
 export default Landing
